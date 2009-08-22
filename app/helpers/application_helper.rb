8c04be7176cffdb5c413fce343ade252dc3fb9e4
Thomas R. Koll <tomk32@tomk32.de>
2009-08-22 10:32:32 +0200

    Migrated existing views to haml Cut down routes Added helper for page_title and meta_title Added basic application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  attr_accessor :page_title
+  def page_title
+    @page_title
+  end
+  def title_meta
+    [t(:'app.title'),page_title].compact.join(' // ')
+  end
 end
